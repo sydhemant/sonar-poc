054f411544920c7eb27c695710e3a401f761b366
Hemant Verma <sydh.hemant@gmail.com>
2020-05-17 11:39:44 +0530

    initial commit

---
+++ src/main/java/com/learntech/sonar/SonarPocApplication.java
@@ -0,0 +1,13 @@
+package com.learntech.sonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learntech/sonar/SonarPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learntech.sonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
